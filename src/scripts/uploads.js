99115f472cb08d5783ba6890b99ca64617185fd2
Mr-G-D <daredinesh00@gmail.com>
2022-01-06 23:27:55 +0530

    post file function added

--- src/scripts/uploads.js
+++ src/scripts/uploads.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const { readdir, stat } = require("fs");
+const poster = require("poster");
+const path = require("path");
+require("dotenv").config();
 
 const uploadParent = document.getElementById("uploads");
 
@@ -16,17 +19,40 @@ readdir("public/assets/", (err, fileNames) => {
         file[1]
       }</h4><h4 class="w-full text-center text-sm">
     ${(fileStat.size / 1048576).toFixed(2)} mb
-    </h4><h4 class="w-full text-right text-sm"><i class="upload fas fa-upload"></i></h4></div>`;
+    </h4><h4 class="w-full text-right text-sm flex justify-around"><button onclick="postFile('${fileName}')"><i id="uploadIcon-${fileName}" class="upload fas fa-upload"></i></button><button onclick="deleteFile()"><i class="far fa-trash-alt"></i></button></h4></div>`;
       uploadParent.appendChild(newFile);
     });
   });
 });
 
-const dropdown = () => {
-  const options = document.getElementById("dropdownOptions");
-  if (options.classList.contains("hidden")) {
-    options.classList.remove("hidden");
-  } else {
-    options.classList.add("hidden");
-  }
+const postFile = (fileName) => {
+  let uploadIcon = document.getElementById(`uploadIcon-${fileName}`);
+  uploadIcon.style.cursor = "not-allowed";
+  uploadIcon.classList.remove("fa-upload");
+  uploadIcon.classList.add("fas", "fa-spinner", "fa-pulse");
+  let filepath = path.join(__dirname, "../../public/assets/", fileName);
+  const url = process.env.SERVER_API;
+  poster.post(
+    filepath,
+    {
+      uploadUrl: url,
+      method: "POST",
+      fileId: "file",
+      fields: {},
+    },
+    function (err, r) {
+      if (err) {
+        uploadIcon.style.cursor = "pointer";
+        uploadIcon.classList.remove("fas", "fa-spinner", "fa-pulse");
+        uploadIcon.classList.add("fas", "fa-upload");
+        console.log(err);
+      } else {
+        uploadIcon.style.cursor = "pointer";
+        uploadIcon.classList.remove("fas", "fa-spinner", "fa-pulse");
+        uploadIcon.classList.add("fas", "fa-check-circle");
+        uploadIcon.style.color = "green";
+        console.log(r);
+      }
+    },
+  );
 };
