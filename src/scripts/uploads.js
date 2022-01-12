412b3fabde549e6d3fc6f6016d7d826ad6e80454
Mr-G-D <daredinesh00@gmail.com>
2022-01-12 21:56:58 +0530

    columns for the files changed

--- src/scripts/uploads.js
+++ src/scripts/uploads.js
@@ -9,21 +9,28 @@ const uploadParent = document.getElementById("uploads");
 
 readdir("public/assets/", (err, fileNames) => {
   fileNames.map((fileName) => {
-    stat(`public/assets/${fileName}`, (err, fileStat) => {
-      let file = fileName.split(".");
-      let newFile = document.createElement("div");
-      newFile.classList.add("flex", "justify-around");
-      newFile.innerHTML = `<div class="w-full bg-gray-200 rounded-2xl mx-10 my-2"><div class="flex flex-row items-start w-full h-full px-10 py-7"><h4 title="${
-        file[0]
-      }" class="w-full text-sm">${
-        file[0].length < 19 ? file[0] : file[0].substring(0, 18) + "..."
-      }</h4><h4 class="w-full text-center text-sm">${
-        file[1]
-      }</h4><h4 class="w-full text-center text-sm">
+    let audio = new Audio(
+      path.join(__dirname, "../../public/assets", fileName),
+    );
+    audio.onloadedmetadata = () => {
+      const duration = audio.duration;
+
+      stat(`public/assets/${fileName}`, (err, fileStat) => {
+        let file = fileName.split(".");
+        let newFile = document.createElement("div");
+        newFile.classList.add("flex", "justify-around");
+        newFile.innerHTML = `<div class="w-full bg-gray-200 rounded-2xl mx-10 my-2"><div class="flex flex-row items-start w-full h-full px-10 py-7"><h4 title="${
+          file[0]
+        }" class="w-full text-sm">${
+          file[0].length < 19 ? file[0] : file[0].substring(0, 18) + "..."
+        }</h4><h4 class="w-full text-center text-sm">${(
+          audio.duration / 60
+        ).toFixed(2)} mins </h4><h4 class="w-full text-center text-sm">
     ${(fileStat.size / 1048576).toFixed(2)} mb
     </h4><h4 class="w-full text-right text-sm flex justify-around"><button onclick="postFile('${fileName}')"><i id="uploadIcon-${fileName}" class="upload fas fa-upload"></i></button><button onclick="deleteFile('${fileName}')"><i class="far fa-trash-alt"></i></button></h4></div>`;
-      uploadParent.appendChild(newFile);
-    });
+        uploadParent.appendChild(newFile);
+      });
+    };
   });
 });
 
